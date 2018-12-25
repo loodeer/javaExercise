b9166904e34e5d03cf6a47d15321aa100f837024
luzuheng <751564347@qq.com>
2018-12-26 00:16:07 +0800

    qrcode'

---
+++ src/main/qrcode/QRCodeUtil.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author loodeer
+ * @date 2018-12-25 23:56
+ */
+public class QRCodeUtil {
+}
---
+++ src/main/qrcode/QrcodeExercise.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author loodeer
+ * @date 2018-12-26 00:10
+ */
+public class QrcodeExercise {
+}
